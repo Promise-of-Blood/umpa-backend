48ea34ba0a713fe524ea61e2c7f68df23c5f0e49
tgoddessana <twicegoddessana1229@gmail.com>
2024-12-16 20:12:43 +0900

    feat: 예제 컨트롤러 추가

--- src/main/java/promiseofblood/umpabackend/UmpaBackendApplication.java
+++ src/main/java/promiseofblood/umpabackend/UmpaBackendApplication.java
@@ -2,9 +2,17 @@ package promiseofblood.umpabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class UmpaBackendApplication {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hi";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UmpaBackendApplication.class, args);
