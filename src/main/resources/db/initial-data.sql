bec2fc0f7ffc8cc0ce910e42d2dc39958147bd3a
tgoddessana <twicegoddessana1229@gmail.com>
2025-01-06 20:21:45 +0900

    Feat: 사용자 타입, 학교 초기화 데이터 추가

--- src/main/resources/db/initial-data.sql
+++ src/main/resources/db/initial-data.sql
@@ -1,3 +1,17 @@
+-- 사용자 타입
+INSERT INTO user_types (name)
+VALUES ('MENTOR'),
+       ('MENTEE');
+
+
+-- 학교
+INSERT INTO university (name)
+VALUES ('서울대학교'),
+       ('연세대학교'),
+       ('고려대학교');
+
+
+-- 전공
 INSERT INTO majors (name)
 VALUES ('피아노'),
        ('작곡'),
@@ -7,9 +21,3 @@ VALUES ('피아노'),
        ('보컬'),
        ('전자음악'),
        ('관악');
-
-
-INSERT INTO university (name)
-VALUES ('서울대학교'),
-       ('연세대학교'),
-       ('고려대학교');
