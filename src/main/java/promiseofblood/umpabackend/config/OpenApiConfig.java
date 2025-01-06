cefdd33a9974852f7015e05d7cfd273077d5d438
tgoddessana <twicegoddessana1229@gmail.com>
2025-01-07 01:13:20 +0900

    Fix: swagger 포트 변경

--- src/main/java/promiseofblood/umpabackend/config/OpenApiConfig.java
+++ src/main/java/promiseofblood/umpabackend/config/OpenApiConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @OpenAPIDefinition(
     servers = {
-      @Server(url = "http://localhost:8080", description = "로컬 서버"),
+      @Server(url = "http://localhost:8000", description = "로컬 서버"),
       @Server(url = "https://tgoddessana.duckdns.org", description = "프로덕션 서버")
     })
 public class OpenApiConfig {
--- src/main/java/promiseofblood/umpabackend/user/controller/UniversityController.java
+++ src/main/java/promiseofblood/umpabackend/user/controller/UniversityController.java
@@ -1,6 +1,26 @@
 package promiseofblood.umpabackend.user.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import promiseofblood.umpabackend.user.dto.UniversityDto;
+import promiseofblood.umpabackend.user.service.UserService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
-public class UniversityController {}
+@RequestMapping("/api/universities")
+@Tag(name = "학교 API")
+public class UniversityController {
+  private final UserService userService;
+
+  @GetMapping("")
+  public ResponseEntity<List<UniversityDto>> universities() {
+    return ResponseEntity.ok(userService.listUniversities());
+  }
+}
