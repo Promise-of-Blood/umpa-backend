f787295676ec278a997c11e7dc06d4fdeac045f6
tgoddessana <twicegoddessana1229@gmail.com>
2025-02-11 11:23:44 +0900

    Refactor: Repository 코드 형식 통일

--- src/main/java/promiseofblood/umpabackend/user/repository/RegionRepository.java
+++ src/main/java/promiseofblood/umpabackend/user/repository/RegionRepository.java
@@ -1,7 +1,9 @@
 package promiseofblood.umpabackend.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import promiseofblood.umpabackend.user.entitiy.region.RegionalLocalGovernment;
 
+@Repository
 public interface RegionRepository extends JpaRepository<RegionalLocalGovernment, Long> {
 }
