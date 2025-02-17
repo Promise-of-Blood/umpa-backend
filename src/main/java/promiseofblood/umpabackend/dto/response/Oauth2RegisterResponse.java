bbaa9f93a8ca22843f9524fc0e3a4c1d7fe2f8e9
tgoddessana <twicegoddessana1229@gmail.com>
2025-02-17 21:04:29 +0900

    Refactor: Enhance Oauth2RegisterResponse by adding userType and wantedColleges fields

--- src/main/java/promiseofblood/umpabackend/dto/response/Oauth2RegisterResponse.java
+++ src/main/java/promiseofblood/umpabackend/dto/response/Oauth2RegisterResponse.java
@@ -1,20 +1,24 @@
 package promiseofblood.umpabackend.dto.response;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import promiseofblood.umpabackend.domain.User;
 
+import java.util.List;
 import java.time.LocalDateTime;
+
+import promiseofblood.umpabackend.domain.College;
+import promiseofblood.umpabackend.domain.User;
 
 @Builder
 @Getter
 @AllArgsConstructor
 public class Oauth2RegisterResponse {
+  private String userType;
   private String name;
   private String profileImageUrl;
   private String major;
+  private List<String> wantedColleges;
   private String oauth2Provider;
 
   private LocalDateTime createdAt;
@@ -22,9 +26,15 @@ public class Oauth2RegisterResponse {
 
   public static Oauth2RegisterResponse of(User user) {
     return Oauth2RegisterResponse.builder()
+            .userType(user.getUserType().getName())
             .name(user.getName())
             .profileImageUrl(user.getProfileImageUrl())
             .major(user.getMajor().getName())
+            .wantedColleges(
+                    user.getWantedColleges().stream()
+                            .map(College::getName)
+                            .toList()
+            )
             .oauth2Provider(user.getSocialUser().getOauth2Provider().getName())
             .createdAt(user.getCreatedAt())
             .updatedAt(user.getUpdatedAt())
