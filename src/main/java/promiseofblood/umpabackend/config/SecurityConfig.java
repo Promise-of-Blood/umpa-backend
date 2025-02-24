a14f0c59d150d323373f88dbbb8b39e487f9b2dc
tgoddessana <twicegoddessana1229@gmail.com>
2025-02-24 19:42:19 +0900

    Feat: JWT 인증 구현

--- src/main/java/promiseofblood/umpabackend/config/SecurityConfig.java
+++ src/main/java/promiseofblood/umpabackend/config/SecurityConfig.java
@@ -1,29 +1,45 @@
 package promiseofblood.umpabackend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import promiseofblood.umpabackend.service.UserService;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+  private final UserService userService;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http
-            .csrf(
-                    AbstractHttpConfigurer::disable
-            )
-            .authorizeHttpRequests(
-                    (
-                            authorizeRequests
-                    )
-                            -> authorizeRequests.anyRequest().permitAll()
-            );
+    http.formLogin(
+            AbstractHttpConfigurer::disable
+    );
+    http.logout(
+            AbstractHttpConfigurer::disable
+    );
+    http.sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    );
+    http.csrf(
+            AbstractHttpConfigurer::disable
+    );
+    http.authorizeHttpRequests(
+            (authorizeRequests) -> authorizeRequests
+                    .anyRequest().permitAll()
+    );
+    http.addFilterBefore(
+            new JwtFilter(userService, "123123123123123123112312312312312312311231231231231231231"),
+            UsernamePasswordAuthenticationFilter.class
+    );
 
     return http.build();
   }
