030d6023ce718fff1478f96826440907b525bba9
goddessana <twicegoddessana1229@gmail.com>
2025-04-28 18:13:03 +0900

    레슨 스타일을 정의하는 Vo(Enum) 정의

--- src/main/java/promiseofblood/umpabackend/domain/vo/LessonStyle.java
+++ src/main/java/promiseofblood/umpabackend/domain/vo/LessonStyle.java
@@ -1,4 +1,19 @@
 package promiseofblood.umpabackend.domain.vo;
 
+import lombok.Getter;
+
+@Getter
 public enum LessonStyle {
+  FACE_TO_FACE("대면"),
+  NON_FACE_TO_FACE("비대면");
+
+  private final String name;
+
+  LessonStyle(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
