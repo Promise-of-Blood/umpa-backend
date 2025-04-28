076cef0f805d3f7c1f194340c204bd4209e15daa
goddessana <twicegoddessana1229@gmail.com>
2025-04-28 22:38:05 +0900

    Fix: OpenApiDefinition 에서 서버 정의하던 코드 제거

--- src/main/java/promiseofblood/umpabackend/config/OpenApiConfig.java
+++ src/main/java/promiseofblood/umpabackend/config/OpenApiConfig.java
@@ -1,26 +1,21 @@
 package promiseofblood.umpabackend.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-    servers = {
-      @Server(url = "http://localhost:8888", description = "로컬 서버"),
-      @Server(url = "https://umpa.tgoddessana.duckdns.org", description = "프로덕션 서버")
-    })
+@OpenAPIDefinition
 public class OpenApiConfig {
 
   @Bean
   public OpenAPI umpaOpenAPI() {
     return new OpenAPI()
-        .info(
-            new io.swagger.v3.oas.models.info.Info()
-                .title("음파 서버 API")
-                .version("0.1.0")
-                .description("음- 하면서 내쉬고, 파- 하면서 들이쉬세요."));
+      .info(
+        new io.swagger.v3.oas.models.info.Info()
+          .title("음파 서버 API")
+          .version("0.1.0")
+          .description("음- 하면서 내쉬고, 파- 하면서 들이쉬세요."));
   }
 }
