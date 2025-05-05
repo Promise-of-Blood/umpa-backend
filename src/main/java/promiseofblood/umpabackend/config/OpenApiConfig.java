2d7cafdbebe9435ab22c0a2e9f7b07408c1d92af
goddessana <twicegoddessana1229@gmail.com>
2025-05-05 19:52:27 +0900

    Fix: springdoc CORS 에러 수정

--- src/main/java/promiseofblood/umpabackend/config/OpenApiConfig.java
+++ src/main/java/promiseofblood/umpabackend/config/OpenApiConfig.java
@@ -1,12 +1,13 @@
 package promiseofblood.umpabackend.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class OpenApiConfig {
 
   @Bean
