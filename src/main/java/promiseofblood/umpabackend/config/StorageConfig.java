e938b257f7add257be6f72b71e2c6ab62e138f8e
goddessana <twicegoddessana1229@gmail.com>
2025-06-16 20:24:20 +0900

    feat: storageservice 인터페이스 및 구현체 추가

---
+++ src/main/java/promiseofblood/umpabackend/config/StorageConfig.java
@@ -0,0 +1,16 @@
+package promiseofblood.umpabackend.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("storage")
+public class StorageConfig {
+
+  private String location = "upload-dir";
+
+}
---
+++ src/main/java/promiseofblood/umpabackend/domain/service/FileSystemStorageService.java
@@ -0,0 +1,102 @@
+package promiseofblood.umpabackend.domain.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+import promiseofblood.umpabackend.config.StorageConfig;
+
+@Service
+public class FileSystemStorageService implements StorageService {
+
+  private final Path rootLocation;
+
+  @Autowired
+  public FileSystemStorageService(StorageConfig storageConfig) {
+
+    if (storageConfig.getLocation().trim().isEmpty()) {
+      throw new RuntimeException("File upload location can not be Empty.");
+    }
+
+    this.rootLocation = Paths.get(storageConfig.getLocation());
+  }
+
+  @Override
+  public void store(MultipartFile file) {
+    try {
+      if (file.isEmpty()) {
+        throw new RuntimeException("Failed to store empty file.");
+      }
+      Path destinationFile = this.rootLocation.resolve(
+        Paths.get(Objects.requireNonNull(file.getOriginalFilename()))).normalize().toAbsolutePath();
+
+      if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+        throw new RuntimeException("Cannot store file outside current directory.");
+      }
+      try (InputStream inputStream = file.getInputStream()) {
+        Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+      }
+
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to store file.", e);
+    }
+  }
+
+  @Override
+  public Stream<Path> loadAll() {
+    try {
+      return Files.walk(this.rootLocation, 1)
+        .filter(path -> !path.equals(this.rootLocation))
+        .map(this.rootLocation::relativize);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to read stored files", e);
+    }
+  }
+
+  @Override
+  public Path load(String filename) {
+    return rootLocation.resolve(filename);
+  }
+
+  @Override
+  public Resource loadAsResource(String filename) {
+    try {
+      Path file = load(filename);
+      Resource resource = new UrlResource(file.toUri());
+      if (resource.exists() || resource.isReadable()) {
+        return resource;
+      } else {
+        throw new RuntimeException("Could not read file: " + filename);
+      }
+      
+    } catch (MalformedURLException e) {
+      throw new RuntimeException("Could not read file: " + filename, e);
+    }
+  }
+
+  @Override
+  public void deleteAll() {
+    FileSystemUtils.deleteRecursively(rootLocation.toFile());
+  }
+
+  @Override
+  public void init() {
+    try {
+      Files.createDirectories(rootLocation);
+    } catch (IOException e) {
+      throw new RuntimeException("Could not initialize storage", e);
+    }
+  }
+}
