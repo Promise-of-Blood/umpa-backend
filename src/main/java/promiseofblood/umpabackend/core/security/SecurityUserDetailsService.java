f43adacbe86f39183ad3d3228f8c79740910105e
goddessana <twicegoddessana1229@gmail.com>
2025-07-06 17:42:58 +0900

    Fix: 인증실패 시 처리되는 UnauthorizedException 던지도록 수정

--- src/main/java/promiseofblood/umpabackend/core/security/SecurityUserDetailsService.java
+++ src/main/java/promiseofblood/umpabackend/core/security/SecurityUserDetailsService.java
@@ -3,8 +3,8 @@ package promiseofblood.umpabackend.core.security;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import promiseofblood.umpabackend.core.exception.UnauthorizedException;
 import promiseofblood.umpabackend.domain.entity.User;
 import promiseofblood.umpabackend.repository.UserRepository;
 
@@ -15,11 +15,11 @@ public class SecurityUserDetailsService implements UserDetailsService {
   private final UserRepository userRepository;
 
   @Override
-  public SecurityUserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+  public SecurityUserDetails loadUserByUsername(String loginId) {
     Optional<User> user = userRepository.findByLoginId(loginId);
 
     if (user.isEmpty()) {
-      throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+      throw new UnauthorizedException("인증에 실패하였습니다.");
     }
 
     return SecurityUserDetails.of(user.get());
