d54321b94e9010039c4ec7faa3a1a52baf0069f2
goddessana <twicegoddessana1229@gmail.com>
2025-07-07 18:57:40 +0900

    Feat: 아이디, 비밀번호 일치하지 않을 시 향샹된 에러 메시지 반환

--- src/main/java/promiseofblood/umpabackend/domain/service/UserService.java
+++ src/main/java/promiseofblood/umpabackend/domain/service/UserService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import promiseofblood.umpabackend.core.exception.UnauthorizedException;
 import promiseofblood.umpabackend.domain.entity.StudentProfile;
 import promiseofblood.umpabackend.domain.entity.TeacherCareer;
 import promiseofblood.umpabackend.domain.entity.TeacherLink;
@@ -200,12 +201,10 @@ public class UserService {
   }
 
   public JwtPairDto generateJwt(String loginId, String password) {
-    User user = userRepository.findByLoginId(loginId)
-      .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
-    if (!passwordEncoder.matches(password, user.getPassword())) {
-      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-    }
+    // 로그인ID 로 사용자를 찾을 수 없거나, 비밀번호가 일치하지 않는 경우 "아이디 혹은 비밀번호가 일치하지 않습니다." 예외를 발생시킵니다.
+    User user = userRepository.findByLoginIdAndPassword(loginId, passwordEncoder.encode(password))
+      .orElseThrow(() -> new UnauthorizedException("아이디 혹은 비밀번호가 일치하지 않습니다."));
 
     return jwtService.createJwtPair(user.getId(), user.getLoginId());
   }
--- src/main/java/promiseofblood/umpabackend/repository/UserRepository.java
+++ src/main/java/promiseofblood/umpabackend/repository/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByLoginId(String loginId);
 
+  Optional<User> findByLoginIdAndPassword(String loginId, String password);
+
 }
