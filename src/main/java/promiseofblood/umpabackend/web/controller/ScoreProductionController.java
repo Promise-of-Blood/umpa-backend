683c447d1712cba647e7727df3c6a768a3bdcc6d
goddessana <twicegoddessana1229@gmail.com>
2025-08-18 20:39:55 +0900

    Feat: 악보제작 서비스 컨트롤러 분리

---
+++ src/main/java/promiseofblood/umpabackend/web/controller/ScoreProductionController.java
@@ -0,0 +1,55 @@
+package promiseofblood.umpabackend.web.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import promiseofblood.umpabackend.application.service.ServiceBoardService;
+import promiseofblood.umpabackend.dto.ScoreProductionServicePostDto;
+import promiseofblood.umpabackend.dto.ScoreProductionServicePostDto.ScoreProductionServicePostResponse;
+import promiseofblood.umpabackend.infrastructure.security.SecurityUserDetails;
+
+@RestController
+@RequestMapping("/api/v1/services")
+@RequiredArgsConstructor
+public class ScoreProductionController {
+
+  private final ServiceBoardService serviceBoardService;
+
+  // ***************
+  // * 악보제작 서비스 *
+  // ***************
+  @Tag(name = "서비스 관리 API(악보 제작)")
+  @PostMapping(value = "/score-production", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @PreAuthorize("isAuthenticated()")
+  public ResponseEntity<ScoreProductionServicePostResponse> registerScoreProduction(
+    @AuthenticationPrincipal SecurityUserDetails securityUserDetails,
+    @ModelAttribute ScoreProductionServicePostDto.ScoreProductionServicePosRequest scoreProductionRequest) {
+
+    String loginId = securityUserDetails.getUsername();
+    ScoreProductionServicePostDto.ScoreProductionServicePostResponse scoreProductionServicePostResponse = serviceBoardService.createScoreProductionServicePost(
+      loginId, scoreProductionRequest);
+
+    return ResponseEntity.ok(scoreProductionServicePostResponse);
+  }
+
+  @Tag(name = "서비스 관리 API(악보 제작)")
+  @GetMapping(path = "/score-production/{id}")
+  public ResponseEntity<ScoreProductionServicePostDto.ScoreProductionServicePostResponse> getScoreProductionServicePost(
+    @PathVariable Long id) {
+
+    ScoreProductionServicePostDto.ScoreProductionServicePostResponse scoreProductionResponse =
+      serviceBoardService.getScoreProductionServicePost(id);
+
+    return ResponseEntity.ok(scoreProductionResponse);
+  }
+
+}
--- src/main/java/promiseofblood/umpabackend/web/controller/ServiceBoardController.java
+++ src/main/java/promiseofblood/umpabackend/web/controller/ServiceBoardController.java
@@ -6,15 +6,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import promiseofblood.umpabackend.application.service.ServiceBoardService;
 import promiseofblood.umpabackend.dto.AccompanimentServicePostDto;
-import promiseofblood.umpabackend.dto.ScoreProductionServicePostDto;
 import promiseofblood.umpabackend.infrastructure.security.SecurityUserDetails;
 
 @RestController
@@ -41,34 +38,6 @@ public class ServiceBoardController {
       serviceBoardService.createAccompanimentServicePost(loginId, accompanimentPostRequest);
 
     return ResponseEntity.ok(accompanimentPostResponse);
-  }
-
-  // ***************
-  // * 악보제작 서비스 *
-  // ***************
-  @Tag(name = "서비스 관리 API(악보 제작)")
-  @PostMapping(value = "/score-production", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  @PreAuthorize("isAuthenticated()")
-  public ResponseEntity<ScoreProductionServicePostDto.ScoreProductionServicePostResponse> registerScoreProduction(
-    @AuthenticationPrincipal SecurityUserDetails securityUserDetails,
-    @ModelAttribute ScoreProductionServicePostDto.ScoreProductionServicePosRequest scoreProductionRequest) {
-
-    String loginId = securityUserDetails.getUsername();
-    ScoreProductionServicePostDto.ScoreProductionServicePostResponse scoreProductionServicePostResponse = serviceBoardService.createScoreProductionServicePost(
-      loginId, scoreProductionRequest);
-
-    return ResponseEntity.ok(scoreProductionServicePostResponse);
-  }
-
-  @Tag(name = "서비스 관리 API(악보 제작)")
-  @GetMapping(path = "/score-production/{id}")
-  public ResponseEntity<ScoreProductionServicePostDto.ScoreProductionServicePostResponse> getScoreProductionServicePost(
-    @PathVariable Long id) {
-
-    ScoreProductionServicePostDto.ScoreProductionServicePostResponse scoreProductionResponse =
-      serviceBoardService.getScoreProductionServicePost(id);
-
-    return ResponseEntity.ok(scoreProductionResponse);
   }
 
 
