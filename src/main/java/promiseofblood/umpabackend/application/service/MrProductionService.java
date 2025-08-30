c20b8ee8752c89246c034a90c9a4156f150d4055
goddessana <twicegoddessana1229@gmail.com>
2025-08-30 17:07:37 +0900

    404를 나타내는 명확한 예외, MrProductionService 용 리포지토리 정의, 없는 id 로 조회 시 예외 발생 처리

--- src/main/java/promiseofblood/umpabackend/application/service/MrProductionService.java
+++ src/main/java/promiseofblood/umpabackend/application/service/MrProductionService.java
@@ -3,22 +3,24 @@ package promiseofblood.umpabackend.application.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import promiseofblood.umpabackend.application.exception.ResourceNotFoundException;
 import promiseofblood.umpabackend.application.query.RetrieveMrServicePostQuery;
 import promiseofblood.umpabackend.domain.entity.MrProductionServicePost;
-import promiseofblood.umpabackend.domain.repository.ServicePostRepository;
+import promiseofblood.umpabackend.domain.repository.MrProductionServicePostRepository;
 import promiseofblood.umpabackend.dto.MrProductionServicePostDto.MrProductionResponse;
 
 @Service
 @RequiredArgsConstructor
 public class MrProductionService {
 
-  private final ServicePostRepository servicePostRepository;
+  private final MrProductionServicePostRepository mrProductionServicePostRepository;
 
   @Transactional(readOnly = true)
   public MrProductionResponse retrieveMrProductionServicePost(RetrieveMrServicePostQuery query) {
 
-    var mrProductionServicePost = (MrProductionServicePost) servicePostRepository.findById(
-      query.id()).get(); // TODO: 예외처리
+    MrProductionServicePost mrProductionServicePost = mrProductionServicePostRepository.findById(
+        query.id())
+      .orElseThrow(() -> new ResourceNotFoundException("해당 ID의 MR 제작 서비스 게시글이 존재하지 않습니다."));
 
     return MrProductionResponse.of(mrProductionServicePost);
   }
