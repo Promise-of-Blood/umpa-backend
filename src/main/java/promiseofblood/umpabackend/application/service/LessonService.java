774f4a7595a373e83adc69bd44095f151b26c170
goddessana <twicegoddessana1229@gmail.com>
2025-09-01 19:49:32 +0900

    레슨 서비스 게시물 생성 기능 추가 및 관련 클래스 구현

---
+++ src/main/java/promiseofblood/umpabackend/application/service/LessonService.java
@@ -0,0 +1,54 @@
+package promiseofblood.umpabackend.application.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import promiseofblood.umpabackend.application.command.CreateLessonServicePostCommand;
+import promiseofblood.umpabackend.application.exception.ResourceNotFoundException;
+import promiseofblood.umpabackend.domain.entity.LessonServicePost;
+import promiseofblood.umpabackend.domain.entity.User;
+import promiseofblood.umpabackend.domain.repository.LessonServicePostRepository;
+import promiseofblood.umpabackend.domain.repository.UserRepository;
+import promiseofblood.umpabackend.domain.vo.ServiceCost;
+
+@Service
+@RequiredArgsConstructor
+public class LessonService {
+
+  private final StorageService storageService;
+  private final UserRepository userRepository;
+  private final LessonServicePostRepository lessonServicePostRepository;
+
+  public void createLessonServicePost(CreateLessonServicePostCommand command) {
+    User user =
+        userRepository
+            .findByLoginId(command.getLoginId())
+            .orElseThrow(() -> new ResourceNotFoundException("사용자를 찾을 수 없습니다."));
+
+    List<String> studioPhotoUrls =
+        command.getStudioPhotos().stream()
+            .map(
+                file ->
+                    storageService.store(file, "service/" + user.getId() + "/lesson/studio-photos"))
+            .toList();
+
+    LessonServicePost lessonServicePost =
+        LessonServicePost.create(
+            user,
+            storageService.store(
+                command.getThumbnailImage(), "service/" + user.getId() + "/lesson"),
+            command.getTitle(),
+            command.getDescription(),
+            ServiceCost.of(command.getServiceCostValue(), command.getServiceCostUnit()),
+            command.getSubject(),
+            command.getAvailableRegions(),
+            command.getAvailableWeekDays(),
+            command.getLessonStyle(),
+            command.isDemoLessonProvided(),
+            command.getDemoLessonCost(),
+            command.getRecommendedTargets(),
+            studioPhotoUrls);
+
+    lessonServicePostRepository.save(lessonServicePost);
+  }
+}
--- src/main/java/promiseofblood/umpabackend/web/controller/LessonServiceController.java
+++ src/main/java/promiseofblood/umpabackend/web/controller/LessonServiceController.java
@@ -1,22 +1,55 @@
 package promiseofblood.umpabackend.web.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import promiseofblood.umpabackend.application.command.CreateLessonServicePostCommand;
+import promiseofblood.umpabackend.application.service.LessonService;
+import promiseofblood.umpabackend.infrastructure.security.SecurityUserDetails;
+import promiseofblood.umpabackend.web.schema.request.CreateLessonServicePostRequest;
 
 @RestController
 @RequestMapping("/api/v1/services")
 @RequiredArgsConstructor
 public class LessonServiceController {
 
+  private final LessonService lessonService;
+
   // *************
   // * 레슨 서비스 *
   // *************
   @Tag(name = "서비스 관리 API(레슨)")
-  @PostMapping("/lesson")
+  @PostMapping(value = "/lesson", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   @PreAuthorize("isAuthenticated()")
-  public void registerLesson() {}
+  public void registerLesson(
+      @AuthenticationPrincipal SecurityUserDetails securityUserDetails,
+      @ModelAttribute @Valid CreateLessonServicePostRequest request) {
+
+    CreateLessonServicePostCommand command =
+        CreateLessonServicePostCommand.builder()
+            .loginId(securityUserDetails.getLoginId())
+            .thumbnailImage(request.getThumbnailImage())
+            .title(request.getTitle())
+            .description(request.getDescription())
+            .serviceCostValue(request.getCost())
+            .serviceCostUnit("곡")
+            .subject(request.getSubject())
+            .availableRegions(request.getAvailableRegions())
+            .availableWeekDays(request.getAvailableWeekDays())
+            .lessonStyle(request.getLessonStyle())
+            .isDemoLessonProvided(request.getIsDemoLessonProvided())
+            .demoLessonCost(request.getDemoLessonCost())
+            .recommendedTargets(request.getRecommendedTargets())
+            .studioPhotos(request.getStudioPhotos())
+            .build();
+
+    lessonService.createLessonServicePost(command);
+  }
 }
