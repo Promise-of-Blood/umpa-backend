3b65105f3f7d514c574e709b1861bd40b1fe0f33
goddessana <twicegoddessana1229@gmail.com>
2025-09-01 19:49:32 +0900

    레슨 서비스 게시물 생성 기능 추가 및 관련 클래스 구현

--- src/main/java/promiseofblood/umpabackend/domain/entity/LessonServicePost.java
+++ src/main/java/promiseofblood/umpabackend/domain/entity/LessonServicePost.java
@@ -7,41 +7,91 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
-import java.time.DayOfWeek;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import promiseofblood.umpabackend.domain.vo.Instrument;
+import lombok.experimental.SuperBuilder;
 import promiseofblood.umpabackend.domain.vo.LessonStyle;
+import promiseofblood.umpabackend.domain.vo.Region;
 import promiseofblood.umpabackend.domain.vo.ServiceCost;
+import promiseofblood.umpabackend.domain.vo.Subject;
+import promiseofblood.umpabackend.domain.vo.WeekDay;
 
 @Entity
 @Table(name = "lesson_service_posts")
 @DiscriminatorValue("LESSON")
+@SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LessonServicePost extends ServicePost {
 
+  // 제목, 설명, 썸네일 이미지, 유저, 리뷰(상속)
+
   @Embedded private ServiceCost serviceCost;
 
-  @ElementCollection
   @Enumerated(EnumType.STRING)
-  private List<Instrument> lessonSubjects;
+  private Subject subject;
 
-  @ElementCollection
+  // 가능한 수업 지역들은 검색가능해야하므로, ElementCollection으로 설정
   @Enumerated(EnumType.STRING)
-  private List<DayOfWeek> availableWeekDays;
+  @ElementCollection
+  private List<Region> availableRegions;
 
+  // 가능한 수업 요일들
+  @Enumerated(EnumType.STRING)
+  private List<WeekDay> availableWeekDays;
+
+  // 수업 스타일(비대면/대면)
   @Enumerated(EnumType.STRING)
   private LessonStyle lessonStyle;
 
-  private boolean isDemoLessonOptionAvailable;
+  // 데모 레슨 제공여부 및 비용
+  private boolean isDemoLessonProvided;
 
-  @ElementCollection private List<String> recommendedTargets;
+  private Integer demoLessonCost;
 
-  @ElementCollection private List<String> studioPhotoUrls;
+  // 추천 대상
+  private List<String> recommendedTargets;
 
-  //  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-  //  private List<LessonCurriculum> curriculum;
+  // 스튜디오 사진들
+  private List<String> studioPhotoUrls;
+
+  // TODO: 커리큘럼 추가
+
+  public static LessonServicePost create(
+      // 공통 필드
+      User user,
+      String thumbnailImageUrl,
+      String title,
+      String description,
+      // 레슨 전용 필드
+      ServiceCost serviceCost,
+      Subject subject,
+      List<Region> availableRegions,
+      List<WeekDay> availableWeekDays,
+      LessonStyle lessonStyle,
+      boolean isDemoLessonProvided,
+      Integer demoLessonCost,
+      List<String> recommendedTargets,
+      List<String> studioPhotoUrls) {
+
+    return LessonServicePost.builder()
+        // 공통 필드
+        .user(user)
+        .thumbnailImageUrl(thumbnailImageUrl)
+        .title(title)
+        .description(description)
+        // 레슨 전용 필드
+        .serviceCost(serviceCost)
+        .subject(subject)
+        .availableRegions(availableRegions)
+        .availableWeekDays(availableWeekDays)
+        .lessonStyle(lessonStyle)
+        .isDemoLessonProvided(isDemoLessonProvided)
+        .demoLessonCost(demoLessonCost)
+        .recommendedTargets(recommendedTargets)
+        .studioPhotoUrls(studioPhotoUrls)
+        .build();
+  }
 
   @Override
   public String getCostAndUnit() {
