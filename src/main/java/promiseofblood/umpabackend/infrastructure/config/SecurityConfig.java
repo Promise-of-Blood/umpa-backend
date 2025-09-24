bfc41a318a18d01b6c04766c71369194bb806083
goddessana <twicegoddessana1229@gmail.com>
2025-09-24 15:04:33 +0900

    CORS 설정 추가

--- src/main/java/promiseofblood/umpabackend/infrastructure/config/SecurityConfig.java
+++ src/main/java/promiseofblood/umpabackend/infrastructure/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpMethod.PUT;
 
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +24,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import promiseofblood.umpabackend.domain.vo.Role;
 import promiseofblood.umpabackend.infrastructure.security.JwtAuthenticationEntryPoint;
 import promiseofblood.umpabackend.infrastructure.security.JwtFilter;
@@ -38,6 +43,8 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
     http.formLogin(AbstractHttpConfigurer::disable);
     http.logout(AbstractHttpConfigurer::disable);
     http.sessionManagement(
@@ -88,6 +95,20 @@ public class SecurityConfig {
   public PasswordEncoder passwordEncoder() {
 
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedMethods(
+        Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
