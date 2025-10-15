0d32db90713dad98a44b421d41e77a98d780d01f
goddessana <twicegoddessana1229@gmail.com>
2025-10-15 20:19:33 +0900

    채팅 임시구현

---
+++ src/main/java/promiseofblood/umpabackend/web/controller/WebSocketController.java
@@ -0,0 +1,29 @@
+package promiseofblood.umpabackend.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import promiseofblood.umpabackend.application.service.ChatService;
+import promiseofblood.umpabackend.dto.ChatDto;
+
+@Controller
+@RequiredArgsConstructor
+public class WebSocketController {
+
+  private final ChatService chatService;
+  private final SimpMessagingTemplate messagingTemplate;
+
+  @MessageMapping("/chat.sendMessage/{roomId}")
+  public void sendMessage(
+      java.security.Principal principal,
+      @DestinationVariable Long roomId,
+      @Payload ChatDto.SendMessageRequest request) {
+    String loginId = principal.getName();
+    ChatDto.ChatMessageResponse response =
+        chatService.sendMessage(loginId, roomId, request.getMessage());
+    messagingTemplate.convertAndSend("/topic/chat/" + roomId, response);
+  }
+}
