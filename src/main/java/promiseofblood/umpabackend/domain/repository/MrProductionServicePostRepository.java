1b7692ef04b9fc9a24cc3a03786dbe6eecf5731c
goddessana <twicegoddessana1229@gmail.com>
2025-11-19 21:14:00 +0900

    더 이상사용되지 않는 서비스의 엔티티들 제거

--- src/main/java/promiseofblood/umpabackend/domain/repository/MrProductionServicePostRepository.java
+++
@@ -1,7 +0,0 @@
-package promiseofblood.umpabackend.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import promiseofblood.umpabackend.domain.entity.MrProductionServicePost;
-
-public interface MrProductionServicePostRepository
-    extends JpaRepository<MrProductionServicePost, Long> {}
--- src/main/java/promiseofblood/umpabackend/domain/repository/ScoreProductionServicePostRepository.java
+++
@@ -1,7 +0,0 @@
-package promiseofblood.umpabackend.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import promiseofblood.umpabackend.domain.entity.ScoreProductionServicePost;
-
-public interface ScoreProductionServicePostRepository
-    extends JpaRepository<ScoreProductionServicePost, Long> {}
